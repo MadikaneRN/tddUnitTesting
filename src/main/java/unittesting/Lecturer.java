aa9103d36fe60667a49b28c5f1dc625fdb6d931f
student <richardmadikane100@gmail.com>
2016-03-04 18:31:05 +0200

    Initial Commit

---
+++ src/main/java/unittesting/Lecturer.java
@@ -0,0 +1,23 @@
+package unittesting;
+
+/**
+ * Created by student on 2016/03/04.
+ */
+public class Lecturer {
+
+    String name;
+    int age;
+
+    public Lecturer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/unittesting/Student.java
@@ -0,0 +1,23 @@
+package unittesting;
+
+/**
+ * Created by student on 2016/03/04.
+ */
+public class Student {
+
+    String  name;
+    String course;
+
+    public Student(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
